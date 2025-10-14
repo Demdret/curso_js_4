dc6513b1975e6d741bdd94bb4f28e2abbf91d1cd
JosephLondono <londonoj888@gmail.com>
2025-10-13 19:43:12 -0500

    unificar estilos para usar solo usar un estilo de card

--- js/main.js
+++ js/main.js
@@ -1,50 +1,48 @@
-const main = document.querySelector('main section');
+const main = document.querySelector("main section");
 
-const input = document.querySelector('#Searching');
-const buttomClean = document.querySelector('#btnClean');
+const input = document.querySelector("#Searching");
+const buttomClean = document.querySelector("#btnClean");
 
-const dataMain = data.map(productos => {
-    return `<div class="card p-4" style="width: 18rem;">
-                <img src="${productos.imagen}" class="card-img-top h-50 w-100" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${productos.modelo}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">${productos.marca} - $${productos.precio}</h6>
-                    <p class="card-text">${productos.descripcion}</p>
-                    <a href="./producto.html?id=${productos.id}" class="btn btn-primary">Ver más...</a>
-                </div>
+const dataMain = data.map((productos) => {
+  return `<div class="producto-card">
+                <img src="${productos.imagen}" alt="${productos.marca} ${productos.modelo}">
+            <h3>${productos.marca} ${productos.modelo}</h3>
+            <p class="descripcion">${productos.descripcion}</p>
+            <p class="precio">$${productos.precio}</p>
+            <a href="./producto.html?id=${productos.id}" class="btn btn-primary">Ver más</a>
             </div>`;
 });
 
 main.innerHTML = dataMain.join("");
 
 function filterData() {
-    const filter = input.value.trim().toLowerCase();
-    const filterData = data.filter(guitarras => guitarras.marca.toLowerCase().includes(filter) ||
-    guitarras.modelo.toLowerCase().includes(filter));
+  const filter = input.value.trim().toLowerCase();
+  const filterData = data.filter(
+    (guitarras) =>
+      guitarras.marca.toLowerCase().includes(filter) ||
+      guitarras.modelo.toLowerCase().includes(filter)
+  );
 
-    main.innerHTML = "";
+  main.innerHTML = "";
 
-        main.innerHTML = filterData.map(producto => `
-            <div class="card p-4" style="width: 18rem;">
-                <img src="${producto.imagen}" class="card-img-top h-50 w-100" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${producto.modelo}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">${producto.marca} - $${producto.precio}</h6>
-                    <p class="card-text">${producto.descripcion}</p>
-                    <a href="./producto.html?id=${producto.id}" class="btn btn-primary">Ver más...</a>
-                </div>
-            </div>
-        `).join("");
+  main.innerHTML = filterData
+    .map(
+      (producto) => `
+            <div class="producto-card">
+                <img src="${producto.imagen}" alt="${producto.marca} ${producto.modelo}">
+            <h3>${producto.marca} ${producto.modelo}</h3>
+            <p class="descripcion">${producto.descripcion}</p>
+            <p class="precio">$${producto.precio}</p>
+            <a href="./producto.html?id=${producto.id}" class="btn btn-primary">Ver más</a>
+            </div>`
+    )
+    .join("");
 }
 
-function mostrarCards(){
-    input.value = "";
-    main.innerHTML = dataMain.join("");
+function mostrarCards() {
+  input.value = "";
+  main.innerHTML = dataMain.join("");
 }
 
 Searching.addEventListener("input", filterData);
 buttomClean.addEventListener("click", mostrarCards);
-
-
-
-
