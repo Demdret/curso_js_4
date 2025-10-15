e2fbf9b08df3541477091f6d89195edd80158223
Jesus Ortiz <manueldelcastillo94@outlook.com>
2025-10-14 21:24:34 -0700

    Ultimos retoques, integracion de 2 elementos mas a la data

--- js/producto.js
+++ js/producto.js
@@ -6,13 +6,13 @@ const guitarras = data.find((guitarras) => guitarras.id === id);
 
 const productoMain = `
                 <div id="height-img" class="card ms-5 me-4 mb-5 mt-5">
-                    <img src="${guitarras.imagen}" class="card-img-top" style ="width:300px;" alt="${guitarras.descripcion}">
+                    <img src="${guitarras.imagen}" class="card-img-top" style ="width:300px;" alt="${guitarras.descripcionLarga}">
                 </div>
                 <div class="card container-product mb-5 mt-5">
                     <div class="container-product-info">
                         <h5 class="card-title">${guitarras.marca}</h5>
                         <h6 class="card-subtitle mb-2 text-muted">${guitarras.modelo}</h6>
-                        <p class="card-text">${guitarras.descripcion}</p>
+                        <p class="card-text">${guitarras.descripcionLarga}</p>
                         <a href="#" class="btn btn-success">$${guitarras.precio}</a>
                     </div>
                 </div>
