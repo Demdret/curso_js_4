2a099f8b0dcbcffe2d9b64c6bf546636dba66836
Jesus Ortiz <manueldelcastillo94@outlook.com>
2025-10-18 15:30:44 -0700

    Creacion de funcion para refactorizar el codigo y mejorar en main.js

--- js/main.js
+++ js/main.js
@@ -3,45 +3,43 @@ const main = document.querySelector("main section");
 const input = document.querySelector("#Searching");
 const buttomClean = document.querySelector("#btnClean");
 
-const dataMain = data.map((productos) => {
-  return `<div class="producto-card p-1">
-                <img src="${productos.imagen}" alt="${productos.marca} ${productos.modelo}">
-            <h3>${productos.marca} ${productos.modelo}</h3>
-            <p class="descripcion">${productos.descripcionCorta}</p>
-            <p class="precio">$${productos.precio}</p>
-            <a href="./producto.html?id=${productos.id}" class="btn btn-primary">Ver más</a>
-            </div>`;
-});
-
-main.innerHTML = dataMain.join("");
-
-function filterData() {
-  const filter = input.value.trim().toLowerCase();
-  const filterData = data.filter(
-    (guitarras) =>
-      guitarras.marca.toLowerCase().includes(filter) ||
-      guitarras.modelo.toLowerCase().includes(filter)
-  );
-
-  main.innerHTML = "";
-
-  main.innerHTML = filterData
+function renderCards(productos) {
+  main.innerHTML = productos
     .map(
       (producto) => `
-            <div class="producto-card">
-                <img src="${producto.imagen}" alt="${producto.marca} ${producto.modelo}">
-            <h3>${producto.marca} ${producto.modelo}</h3>
-            <p class="descripcion">${producto.descripcionCorta}</p>
-            <p class="precio">$${producto.precio}</p>
-            <a href="./producto.html?id=${producto.id}" class="btn btn-primary">Ver más</a>
-            </div>`
+        <div class="producto-card p-1">
+          <img src="${producto.imagen}" alt="${producto.marca} ${producto.modelo}">
+          <h3>${producto.marca} ${producto.modelo}</h3>
+          <p class="descripcion">${producto.descripcionCorta}</p>
+          <p class="precio">$${producto.precio}</p>
+          <a href="./producto.html?id=${producto.id}" class="btn btn-primary">Ver más</a>
+        </div>`
     )
     .join("");
 }
 
+renderCards(data);
+
+function filterData() {
+  const filter = input.value.trim().toLowerCase();
+  let cantidadLetras = input.value.length;
+  if(cantidadLetras >= 3){
+    const filterData = data.filter(
+    (guitarras) =>
+      guitarras.marca.toLowerCase().includes(filter) ||
+      guitarras.modelo.toLowerCase().includes(filter)
+    );
+
+    renderCards(filterData);
+  } else {
+    renderCards(data);
+  }
+  
+}
+
 function mostrarCards() {
   input.value = "";
-  main.innerHTML = dataMain.join("");
+  renderCards(data);
 }
 
 Searching.addEventListener("input", filterData);
