f2ee70508cdc13756a2b9dc843bb38a9ed89536f
Joseph Londoño <londonoj888@gmail.com>
2025-10-22 18:40:52 -0500

    Agregar en el navbar la logica de enviar en que archivo me encuentro para el login.js

--- js/navbar.js
+++ js/navbar.js
@@ -27,3 +27,29 @@ for (const route of routes) {
   ul.innerHTML += `<li class="nav-item">
   <a class="nav-link active" href="${route.path}.html">${route.name}</a></li>`;
 }
+
+const dataUser = document.querySelector("#dataUser");
+const email = localStorage.getItem("email");
+const pathname = window.location.pathname.split("/")[1];
+const search = new URLSearchParams(window.location.search).get("id");
+
+console.log(search);
+email
+  ? (dataUser.innerHTML = `Hola! ${localStorage.getItem("email")}
+      <button
+          type="button"
+          class="btn btn-danger"
+          onclick="localStorage.removeItem('email'); window.location.reload();"
+        >
+          Cerrar Sesion
+      </button>`)
+  : (dataUser.innerHTML = `
+      <button
+          type="button"
+          class="btn btn-primary"
+          onclick="location.href = './login.html?redirect=${pathname}${
+      search ? "&id=" + search : ""
+    }'"
+        >
+          Iniciar Sesion
+      </button>`);
